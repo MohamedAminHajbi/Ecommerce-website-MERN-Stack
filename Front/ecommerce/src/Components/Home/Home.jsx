6ee26e2e414f0e8ff3af0ae5c167a8ef84f3d27e
unknown <aminhajbi116@gmail.com>
2023-07-03 14:07:21 +0100

    Carousel

--- Front/ecommerce/src/Components/Home/Home.jsx
+++ Front/ecommerce/src/Components/Home/Home.jsx
@@ -1,14 +1,38 @@
 import React from 'react'
+import Slider from 'react-slick'
 import './Home.css'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const Home = () => {
+  const carouselItems = [
+    { id: 1, imageUrl: 'image1.jpg', caption: 'Image 1' },
+    { id: 2, imageUrl: 'image2.jpg', caption: 'Image 2' },
+    { id: 3, imageUrl: 'image3.jpg', caption: 'Image 3' },
+    // Add more items as needed
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div className='home-container'>
       <div className="home-main-content home-content">
-        sdqffqdsdsqf
+      <Slider {...settings}>
+        {carouselItems.map((item) => (
+          <div key={item.id}>
+            <img src={item.imageUrl} alt={item.caption} />
+            <p>{item.caption}</p>
+          </div>
+        ))}
+      </Slider>
       </div>
       <div className="home-aside home-content">
-dsfdsfdqsfdqsf
+        dsfdsfdqsfdqsf
       </div>
     </div>
   )
