215d0d282994479b380421a7cb6be343c7b22653
unknown <aminhajbi116@gmail.com>
2023-07-10 13:12:24 +0100

    Routing

--- Front/ecommerce/src/App.js
+++ Front/ecommerce/src/App.js
@@ -8,16 +8,18 @@ import SuccessSend from "./Components/SuccessSend/SuccessSend";
 import Index from "./Components/Index/Index";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
 
 function App() {
   
   return (
     <Router>
         <Routes>
-          <Route path="/" element={<Register />} />
+          <Route path="/" element={<Index />} />
           <Route path="/login" element={<Login />} />
           <Route path="/home" element={<Sidebar><Home /></Sidebar>} />
           <Route path="/contact" element={<Sidebar><Contact/></Sidebar>} />
+          <Route path="*" element={<ErrorPage />} />
         </Routes>
     </Router>
   );
--- Front/ecommerce/src/Components/ErrorPage/ErrorPage.jsx
+++ Front/ecommerce/src/Components/ErrorPage/ErrorPage.jsx
@@ -4,14 +4,15 @@ import './ErrorPage.css'
 
 const ErrorPage = () => {
   return (
-    <div className='error-page'>
+    <div className="error-container">
+        <div className='error-page'>
         <div className="content">
             <h1>404</h1>
             <h4>Opps! Page not found</h4>
             <p>Sorry, the page you're looking for doesn't exist. If you think something is broke, report a problem.</p>
-            <Link href="#">Return Home</Link>
+            <Link href="#" className='error-btn'>Return Home</Link>
         </div>
-
+        </div>
     </div>
   )
 }
