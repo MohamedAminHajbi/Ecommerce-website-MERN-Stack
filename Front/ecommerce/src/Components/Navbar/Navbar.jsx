a137c4f75ebc20e47371160dd3bfde1aebeb07ee
unknown <aminhajbi116@gmail.com>
2023-07-12 13:53:19 +0100

    updates

--- Front/ecommerce/src/Components/Navbar/Navbar.jsx
+++ Front/ecommerce/src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import LogoutButton from './LogoutButton/LogoutButton'
 import './Navbar.css'
 import LoginButton from './LoginButton/LoginButton'
+import Signup from './Signup/Signup'
 
 const Navbar = () => {
   return (
     <div className='navbar-container'>
       <LoginButton/>
+      <Signup/>
       <LogoutButton/>
     </div>
   )
